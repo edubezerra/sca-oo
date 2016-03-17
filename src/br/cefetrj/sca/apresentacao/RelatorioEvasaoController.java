7abd49a6ce9b0d6eb9dbc541b4ceed66b0019d82
Leonardo Castelo Branco Oliveira <lcbo_7794@hotmail.com>
2016-03-17 13:31:24 -0300

    RelatorioEvasaoController

---
+++ src/br/cefetrj/sca/apresentacao/RelatorioEvasaoController.java
@@ -0,0 +1,69 @@
+package br.cefetrj.sca.apresentacao;
+
+import java.util.logging.Logger;
+import javax.servlet.http.HttpServletRequest;
+
+import org.json.JSONArray;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import br.cefetrj.sca.service.RelatorioEvasaoService;
+
+@Controller
+@SessionAttributes("login")
+@RequestMapping("/relatorioEvasao")
+public class RelatorioEvasaoController{
+
+	protected Logger logger = Logger
+			.getLogger(RelatorioEvasaoController.class.getName());
+
+	@Autowired
+	private RelatorioEvasaoService service;
+	
+	@RequestMapping(value = "/{*}", method = RequestMethod.GET)
+	public String get(Model model) {
+		model.addAttribute("error", "Erro: página não encontrada.");
+		return "/homeView";
+	}
+
+	@RequestMapping(value = "/homeEvasao", method = RequestMethod.GET)
+	public String homeEvasao(HttpServletRequest request, Model model) {
+         try{
+        	 
+        	 return "/relatorioEvasao/homeEvasaoView";
+        	 
+         }catch(Exception exc){
+        	 
+        	 model.addAttribute("error", exc.getMessage());
+ 			 return "/homeView";
+        	 
+         }
+	
+	}
+	
+	@RequestMapping(value = "/relatorioEvasao", method = RequestMethod.POST)
+	public String relatorioEvasao(HttpServletRequest request, Model model ,@RequestParam String curso,
+			@RequestParam String periodoLetivo) {
+         try{
+        	 
+        	 JSONArray data = service.createDataResponse(curso, periodoLetivo);
+        	 model.addAttribute("data", data.toString());
+        	 return "/relatorioEvasao/homeEvasaoView";
+        	 
+         }catch(Exception exc){
+        	 
+        	 model.addAttribute("error", exc.getMessage());
+ 			 return "/homeView";
+        	 
+         }
+	
+		
+	}
+
+
+
+}
